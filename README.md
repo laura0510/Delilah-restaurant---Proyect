1132083f6f679c9939a080f046279d7a2d04ce3c
laura <lclopez104@gmail.com>
2020-06-11 22:03:48 -0500

    readme

--- README.md
+++ README.md
@@ -1,11 +1,45 @@
-#DELILAH RESTÓ
-##Proyecto final DWFS - Acámica
+# DELILAH RESTÓ
+## Proyecto final DWFS - Acámica
 El objetivo del proyecto es crear una API realizada con node.js para administrar pedidos de restaurante.
 Esto incluye la creación de servidor, autenticación de usuarios y conexión con base de datos MYSQL.
 
 
-##Dependecias usadas
-"eslint-config-prettier": "^6.11.0",
+### clonar el repositorio 
+```bash
+ git clone https://github.com/laura0510/Delilah-restaurant---Proyect
+
+```
+
+### Instalación del proyecto 
+
+## instalación node modules
+
+Realizar los siguientes pasos para inicializar la instalación de node en el proyecto
+
+```bash
+npm install
+```
+
+## configuración
+1. Crear base de datos desde MYSQL
+2. Ejecutar el servidor del puerto 3000
+3. Crear tablas y sus relaciones, usando las **queries** existentes en el archivo Scripts/create database.sql
+4. Crear "models" y "routes de usuario, producto y pedido.
+5. verificar archivo en postman
+
+## Instalación de base de datos
+ Correr los siguientes scripts (usuarios, pedidos, productos) para crear la BD
+
+ CREATE TABLE(
+    id INT NOT NULL AUTO_INCREMENT,
+)
+
+## Correr la API 
+```bash
+node index.js
+```
+## Dependecias usadas
+    "eslint-config-prettier": "^6.11.0",
     "express": "^4.17.1",
     "jsonwebtoken": "^8.5.1",
     "morgan": "^1.10.0",
@@ -15,13 +49,4 @@ Esto incluye la creación de servidor, autenticación de usuarios y conexión co
     "sequelize": "^5.21.7",
 
 
-##CONFIGURACION
-Crear base de datos desde MYSQL
-Ejecutar el servidor del puerto 3000
 
-como middlewares: 
-1.validarToken: Donde se verifica el token, rol administrador y rol cliente
-2.verificarUsuario: autorización
-
-correr la API 
-node index.js
--- models/Usuario.js
+++ models/Usuario.js
@@ -5,41 +5,41 @@ const sequelize = new Sequelize("mysql://master_laura:Acamica123@automosaiko.tk:
 const Usuario = {};
 
 Usuario.crear = async (usuario, nombre, email, telefono, direccion, contrasena, es_admin) => {
-    return await sequelize.query("INSERT INTO usuarios(usuario, nombre, email, telefono, direccion, contrasena, es_admin) VALUES (?, ?, ?, ?, ?, ?, ?)", {
-        replacements: [usuario, nombre, email, telefono, direccion, contrasena, es_admin]
-    });
+	return await sequelize.query("INSERT INTO usuarios(usuario, nombre, email, telefono, direccion, contrasena, es_admin) VALUES (?, ?, ?, ?, ?, ?, ?)", {
+		replacements: [usuario, nombre, email, telefono, direccion, contrasena, es_admin]
+	});
 };
 
 Usuario.obtenerTodos = async () => {
-    return await sequelize.query("SELECT usuario, nombre, email, telefono, direccion FROM usuarios", {
-        type: sequelize.QueryTypes.SELECT
-    });
+	return await sequelize.query("SELECT usuario, nombre, email, telefono, direccion FROM usuarios", {
+		type: sequelize.QueryTypes.SELECT
+	});
 };
 
 Usuario.autenticar = async (usuario, contrasena) => {
-    return await sequelize.query("SELECT id_usuario,usuario, nombre, email, telefono, direccion, es_admin FROM usuarios WHERE usuario=? AND contrasena=?", {
-        type: sequelize.QueryTypes.SELECT,
-        replacements: [usuario, contrasena]
-    });
+	return await sequelize.query("SELECT id_usuario,usuario, nombre, email, telefono, direccion, es_admin FROM usuarios WHERE usuario=? AND contrasena=?", {
+		type: sequelize.QueryTypes.SELECT,
+		replacements: [usuario, contrasena]
+	});
 };
 
 Usuario.actualizar = async (usuario, nombre, email, telefono, direccion, contrasena, es_admin, oldUser) => {
-    return await sequelize.query("UPDATE usuarios SET usuario = ?, nombre = ?, email = ?, telefono = ?, direccion = ?, contrasena = ?, es_admin = ? where usuario = ? ", {
-        replacements: [usuario, nombre, email, telefono, direccion, contrasena, es_admin, oldUser]
-    });
+	return await sequelize.query("UPDATE usuarios SET usuario = ?, nombre = ?, email = ?, telefono = ?, direccion = ?, contrasena = ?, es_admin = ? where usuario = ? ", {
+		replacements: [usuario, nombre, email, telefono, direccion, contrasena, es_admin, oldUser]
+	});
 };
 
 Usuario.eliminar = async (usuario) => {
-    return await sequelize.query("delete from usuarios where usuario = ? ", {
-        replacements: [usuario]
-    });
+	return await sequelize.query("delete from usuarios where usuario = ? ", {
+		replacements: [usuario]
+	});
 };
 
 Usuario.buscarXUsuario = async (usuario) => {
-    return await sequelize.query("SELECT id_usuario,usuario, nombre, email, telefono, direccion, es_admin FROM usuarios where usuario = ?", {
-        type: sequelize.QueryTypes.SELECT,
-        replacements: [usuario]
-    });
+	return await sequelize.query("SELECT id_usuario,usuario, nombre, email, telefono, direccion, es_admin FROM usuarios where usuario = ?", {
+		type: sequelize.QueryTypes.SELECT,
+		replacements: [usuario]
+	});
 };
 
 module.exports = Usuario;
